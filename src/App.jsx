1743f5380bec07dd64b1f2ee6c8aaac7902923aa
webQbe <webcubesolutionslk@gmail.com>
2025-01-29 20:47:23 +0530

    Displayed image author name

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ const App = () => {
         // Create Card to Display Image
         <div class="max-w-sm rounded overflow-hidden shadow-lg">
             <img src="https://picsum.photos/200/300" alt="" className="w-full" />
+            <div className="px-6 py-4">
+                <div className="font-bold text-purple-500 text-xl mb-2">
+                    Photo by John Doe
+                </div>
+            </div>
         </div>
     )
 }
