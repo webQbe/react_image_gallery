da0b54a1d651f4fa78758a9b10310aefe5d70949
webQbe <webcubesolutionslk@gmail.com>
2025-02-02 15:46:59 +0530

    setText() updates text state with user input

--- src/components/ImageSearch.jsx
+++ src/components/ImageSearch.jsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+
 
 const ImageSearch = () => {
+    const [text, setText] = useState(''); // Set text state to empty by default
+
   return (
+    // setText() updates text state with user input
     <div>
         <div className="max-w-sm rounded overflow-hidden my-10 mx-auto">
             <form className="w-full max-w-sm">
                 <div className="flex items-center border-b border-teal-500 py-2">
-                    <input type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
+                    <input onChange={e => setText(e.target.value)} type="text" className="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none" 
                     placeholder='Search Image Term...' />
                     <button className="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-white py-1 px-2 rounded" type="submit">
                         Search
